ddd7ccd643b841bf54532ede4547dd11fcf3271e
OlutechICTresource <59420028+OlutechICTresource@users.noreply.github.com>
2020-02-12 11:37:03 +0000

    uploading site
    uploading website

---
+++ olutechctresources/css/script.js
@@ -0,0 +1,9 @@
+// Introduction to scripts:
+
+/* INTRODUCTION*/
+
+console.log('This is multiplication of 2367777 by 51');
+console.log(2367777*51);
+
+document.write('This is multiplication of 2367777 by 51 is ');
+document.log(2367777*51);
---
+++ olutechctresources/script.js
@@ -0,0 +1,21 @@
+// Introduction to scripts:
+
+/* INTRODUCTION*/
+
+console.log('This is multiplication of 2367777 by 51');
+console.log(2367777*51);
+
+document.write('This is multiplication of 2367777 by 51 is');
+document.write(2367777*51);
+
+prompt('who are you');
+var name = prompt('what is your name');
+
+console.log('This client name is' + name);
+document.write('This client name is' + name);
+
+console.log('up')
+var items["orange", 3, 4, 5, 6, 7];
+console.log(items);
+
+console.log('bottom')
